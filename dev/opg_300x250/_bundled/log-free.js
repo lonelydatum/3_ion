5851249d075be44477d92f05e882871222c08434
Gar <gar@lonelydatu.com>
2019-12-12 09:11:41 -0500

    on

--- dev/opg_300x250/_bundled/log-free.js
+++ dev/opg_300x250/_bundled/log-free.js
@@ -6,36 +6,37 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 var start = function start() {
 
 	var MASK_TIME = .6;
-	var mask = { clip: 'rect(0px, 0px, 500px, 0px)', ease: Power3.easeOut };
+	var mask = { clip: 'rect(100px, 600px, 500px, 0px)', opacity: 1, ease: Power1.easeOut };
 
 	TweenLite.to(".hero", 15, { scale: .5, x: 0, y: 0 });
 
 	var tl = new TimelineMax();
 	tl.set('.frame1', { opacity: 1 });
+	tl.set('.masker', { opacity: 0 });
 
 	tl.to('.t1', .3, { opacity: 0 }, "+=1.7");
-	tl.from('.t2a', MASK_TIME, _extends({}, mask), "+=.2");
-	tl.from('.t2b', MASK_TIME, _extends({}, mask), '-=.2');
+	tl.to('.t2a', MASK_TIME, _extends({}, mask), "+=.2");
+	tl.to('.t2b', MASK_TIME, _extends({}, mask), '-=.2');
 
 	tl.to('.t2', .3, { opacity: 0 }, "+=1.5");
 
-	tl.from('.t3a', MASK_TIME, _extends({}, mask), "+=.2");
-	tl.from('.t3b', MASK_TIME, _extends({}, mask), '-=.2');
+	tl.to('.t3a', MASK_TIME, _extends({}, mask), "+=.2");
+	tl.to('.t3b', MASK_TIME, _extends({}, mask), '-=.2');
 
 	// tl.add('t3Shift', "+=1")
 	// tl.to(['.t3a', '.t3b'], .3, {x:-4, y:-15}, 't3Shift')
 
 	tl.add('green', "+=1");
 
-	tl.from('.greenbar', .3, { opacity: 0 }, 'green');
-	tl.to('.greenbar', .3, { x: 50 }, 'green');
-	tl.from('.t3c', MASK_TIME, { clip: 'rect(0px, 600px, 500px, 600px)', ease: Power3.easeOut }, 'green');
+	// tl.from('.greenbar', .3, {opacity:0}, 'green')
+	// tl.to(, .3, {x:50}, 'green')
+	tl.from(['.t3c', '.greenbar'], .3, { opacity: 0 }, 'green');
 
-	tl.to(".t3", .3, { opacity: 0 }, "+=2");
+	tl.to(".t3", .3, { opacity: 0 }, "+=2.5");
 
 	tl.add('end', "+=.1");
 	tl.to(".t1", .3, { opacity: 1 }, "end");
-	tl.to(".cover", 1, { opacity: .7 }, "end-=1");
+	tl.to(".cover.static", 1, { opacity: .7 }, "end-=1");
 
 	// tl.gotoAndPlay("green")
 };
--- dev/opg_300x250/_js/main.js
+++ dev/opg_300x250/_js/main.js
@@ -2,22 +2,24 @@ const start = ()=>{
 
 	
 	const MASK_TIME = .6
-	const mask = {clip: `rect(0px, 0px, 500px, 0px)`, ease:Power3.easeOut}
+	const mask = {clip: `rect(100px, 600px, 500px, 0px)`, opacity:1, ease:Power1.easeOut}
 
 	TweenLite.to(".hero", 15, {scale:.5, x:0, y:0})
 
 
+
 	const tl = new TimelineMax()
 	tl.set('.frame1', {opacity:1})
+	tl.set('.masker', {opacity:0})
 	
 	tl.to('.t1', .3, {opacity:0}, "+=1.7")
-	tl.from('.t2a', MASK_TIME, {...mask}, "+=.2" )
-	tl.from('.t2b', MASK_TIME, {...mask}, '-=.2')
+	tl.to('.t2a', MASK_TIME, {...mask}, "+=.2" )
+	tl.to('.t2b', MASK_TIME, {...mask}, '-=.2')
 
 	tl.to('.t2', .3, {opacity:0}, "+=1.5")
 
-	tl.from('.t3a', MASK_TIME, {...mask}, "+=.2" )
-	tl.from('.t3b', MASK_TIME, {...mask}, '-=.2')
+	tl.to('.t3a', MASK_TIME, {...mask}, "+=.2" )
+	tl.to('.t3b', MASK_TIME, {...mask}, '-=.2')
 
 
 	// tl.add('t3Shift', "+=1")
@@ -26,15 +28,15 @@ const start = ()=>{
 	
 	tl.add('green', "+=1")
 	
-	tl.from('.greenbar', .3, {opacity:0}, 'green')
-	tl.to('.greenbar', .3, {x:50}, 'green')
-	tl.from('.t3c', MASK_TIME, {clip: `rect(0px, 600px, 500px, 600px)`, ease:Power3.easeOut}, 'green')
+	// tl.from('.greenbar', .3, {opacity:0}, 'green')
+	// tl.to(, .3, {x:50}, 'green')
+	tl.from(['.t3c', '.greenbar'], .3, {opacity:0}, 'green')
 
-	tl.to(".t3", .3, {opacity:0}, "+=2")
+	tl.to(".t3", .3, {opacity:0}, "+=2.5")
 
 	tl.add('end', "+=.1")
 	tl.to(".t1", .3, {opacity:1}, "end")
-	tl.to(".cover", 1, {opacity:.7}, "end-=1")
+	tl.to(".cover.static", 1, {opacity:.7}, "end-=1")
 
 	// tl.gotoAndPlay("green")
 }
